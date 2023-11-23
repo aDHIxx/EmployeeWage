cc0ca13723a4389a938fc451b55da9a1303ebb67
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-23 16:05:52 +0530

    Added: UseCase 1: Checking whether employee is present or absent

--- employeeWage/src/employeeWageComp.java
+++ employeeWage/src/employeeWageComp.java
@@ -1,5 +1,13 @@
 public class employeeWageComp {
+    /*@desc: Using Random to check if employee is present or absent
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int attendanceStatus = (int) (Math.random() * 2);
+        if (attendanceStatus == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
-}
+    }
