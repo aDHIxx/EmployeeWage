704d67e388ded2b72178509e6ae609fae7884245
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-23 16:12:34 +0530

    Added: UseCase 2: Calculate daily wage of employee

--- employeeWage/src/employeeWageComp.java
+++ employeeWage/src/employeeWageComp.java
@@ -1,13 +1,27 @@
 public class employeeWageComp {
-    /*@desc: Using Random to check if employee is present or absent
+    /**
+     * @name: calculateDailyWage
+     * @i/p: hoursWorked - the number of hours worked by the employee
+     * @return/ o/p: the calculated daily wage
      */
+    public static double calculateDailyWage(int hoursWorked, double hourlyRate) {
+        return hoursWorked * hourlyRate;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        double hourlyRate = 20.00;
+        int hoursWorked = 8;
+
         int attendanceStatus = (int) (Math.random() * 2);
+
         if (attendanceStatus == 1) {
             System.out.println("Employee is Present");
+            double dailyWage = calculateDailyWage(hoursWorked, hourlyRate);
+            System.out.println("Daily Employee Wage: " + dailyWage);
         } else {
             System.out.println("Employee is Absent");
+            System.out.println("Daily Employee Wage: 0");
         }
     }
     }
