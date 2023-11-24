0d0d669a7130ab75fe5c368d26aeec0ce0d0570a
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-24 21:39:45 +0530

    Added:UseCase 7: Refactor UC6 to create class method to compute monthly employee wage

--- employeeWage/src/employeeWageComp.java
+++ employeeWage/src/employeeWageComp.java
@@ -1,3 +1,9 @@
+import java.util.Random;
+
+/*
+    * @name: employeeWageComp
+    * @desc: calculates the monthly wage of an employee
+*/
 public class employeeWageComp {
     private static final int FULL_TIME = 1;
     private static final int PART_TIME = 2;
@@ -7,31 +13,37 @@ public class employeeWageComp {
     private static final int MAX_WORKING_HOURS = 100;
     private static final int MAX_WORKING_DAYS = 20;
 
-    /**
+    private double totalWage = 0;
+    private int totalWorkingHours = 0;
+    private int totalWorkingDays = 0;
+
+    /*
      * @name: calculateDailyWage
+     * @desc: calculates the daily wage of an employee
      * @i/p: hoursWorked - the number of hours worked by the employee
      * @return/ o/p: the calculated daily wage
      */
-    private static double calculateDailyWage(int hoursWorked) {
+    private double calculateDailyWage(int hoursWorked) {
         return hoursWorked * HOURLY_WAGE;
     }
 
-    /**
+    /*
      * @name: generateAttendance
-     * @return/ o/p: the attendance status (FULL_TIME, PART_TIME, or ABSENT)
+     * @desc: generates the attendance status of an employee
+     * @i/p: none
+     * @return/ o/p: the attendance status of an employee (full time : 1, part time: 2, or absent: 0)
      */
-    private static int generateAttendance() {
-        return (int) (Math.random() * 3) + 1;
+    private int generateAttendance() {
+        return new Random().nextInt(3) + 1;
     }
 
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to Employee Wage Computation Program");
-
-        double totalWage = 0;
-        int totalWorkingHours = 0;
-        int totalWorkingDays = 0;
-
+    /*
+     * @name: computeMonthlyWage
+     * @desc: computes the monthly wage of an employee
+     * @param: none
+     * @return/ o/p: the total monthly wage, working hours, and working days
+     */
+    public void computeMonthlyWage() {
         while (totalWorkingHours < MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
             int attendanceStatus = generateAttendance();
             int hoursWorked = 0;
@@ -60,4 +72,10 @@ public class employeeWageComp {
         System.out.println("Total Working Days: " + totalWorkingDays);
     }
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        // Create an object of employeeWageComp class
+        employeeWageComp wageCalculator = new employeeWageComp();
+        wageCalculator.computeMonthlyWage();
+    }
 }
