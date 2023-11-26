039d3a3b3eb1294d41688777b8627ccc59d99099
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-27 03:27:57 +0530

    Added: Manage employee wage of multiple companies

--- employeeWage/src/employeeWageComp.java
+++ employeeWage/src/employeeWageComp.java
@@ -1,79 +1,117 @@
 import java.util.Random;
-
 /*
- *@description: Employee Wage Computation Program
+ * @name   : employeeWageComp
+ * @desc   : calculate monthly wage of an employee for multiple companies
  */
-class EmpWageBuilder {
+class CompanyEmpWage {
+    private final String companyName;
     private final int maxWorkingHours;
     private final int maxWorkingDays;
     private final int fullTimeHours;
     private final int partTimeHours;
     private final int hourlyWage;
-    private double totalWage;
-    private int totalWorkingHours;
-    private int totalWorkingDays;
-
     /*
-     *@desc: Parameterized Constructor
-     *@param: maxWorkingHours, maxWorkingDays, fullTimeHours, partTimeHours, hourlyWage
+     * @name   : CompanyEmpWage
+     * @desc   : constructor for CompanyEmpWage class
      */
-    public EmpWageBuilder(int maxWorkingHours, int maxWorkingDays, int fullTimeHours, int partTimeHours, int hourlyWage) {
+    public CompanyEmpWage(String companyName, int maxWorkingHours, int maxWorkingDays, int fullTimeHours, int partTimeHours, int hourlyWage) {
+        this.companyName = companyName;
         this.maxWorkingHours = maxWorkingHours;
         this.maxWorkingDays = maxWorkingDays;
         this.fullTimeHours = fullTimeHours;
         this.partTimeHours = partTimeHours;
         this.hourlyWage = hourlyWage;
-        this.totalWage = 0;
-        this.totalWorkingHours = 0;
-        this.totalWorkingDays = 0;
+    }
+    //getters
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public int getMaxWorkingHours() {
+        return maxWorkingHours;
+    }
+
+    public int getMaxWorkingDays() {
+        return maxWorkingDays;
+    }
+
+    public int getFullTimeHours() {
+        return fullTimeHours;
+    }
+
+    public int getPartTimeHours() {
+        return partTimeHours;
+    }
+
+    public int getHourlyWage() {
+        return hourlyWage;
+    }
+}
+/*
+ * @name   : EmpWageBuilder
+ * @desc   : class to compute monthly wage of an employee for multiple companies
+ */
+class EmpWageBuilder {
+    private final CompanyEmpWage[] companies;
+
+    public EmpWageBuilder(CompanyEmpWage[] companies) {
+        this.companies = companies;
     }
     /*
-     *@name: generateAttendance
-     *@description: Generates a random number between 1 and 3
-     *@return: 1 for Full Time, 2 for Part Time, 3 for Absent
+     * @name   : generateAttendance
+     * @desc   : generate random number between 1 and 3
+     * @returns: random number
      */
     private int generateAttendance() {
         return new Random().nextInt(3) + 1;
     }
     /*
-     *@name: calculateDailyWage
-     *@description: Calculates the daily wage of the employee
-     *@param: hoursWorked
-     *@return: dailyWage
+     * @name   : calculateDailyWage
+     * @desc   : calculate daily wage of an employee
+     * @params : hoursWorked, hourlyWage
+     * @returns: daily wage
      */
-    private double calculateDailyWage(int hoursWorked) {
+    private double calculateDailyWage(int hoursWorked, int hourlyWage) {
         return hoursWorked * hourlyWage;
     }
     /*
-     *@name: computeMonthlyWage
-     *@description: Computes the monthly wage of the employee
+     * @name   : computeMonthlyWage
+     * @desc   : compute monthly wage of an employee for multiple companies
      */
     public void computeMonthlyWage() {
-        while (totalWorkingHours < maxWorkingHours && totalWorkingDays < maxWorkingDays) {
-            int attendanceStatus = generateAttendance();
-            int hoursWorked = 0;
+        for (CompanyEmpWage company : companies) {
+            double totalWage = 0;
+            int totalWorkingHours = 0;
+            int totalWorkingDays = 0;
 
-            switch (attendanceStatus) {
-                case 1:
-                    System.out.println("Day " + (totalWorkingDays + 1) + ": Employee is Present - Full Time");
-                    hoursWorked = fullTimeHours;
-                    break;
-                case 2:
-                    System.out.println("Day " + (totalWorkingDays + 1) + ": Employee is Present - Part Time");
-                    hoursWorked = partTimeHours;
-                    break;
-                default:
-                    System.out.println("Day " + (totalWorkingDays + 1) + ": Employee is Absent");
+            System.out.println("Company: " + company.getCompanyName());
+            while (totalWorkingHours < company.getMaxWorkingHours() && totalWorkingDays < company.getMaxWorkingDays()) {
+                int attendanceStatus = generateAttendance();
+                int hoursWorked = 0;
+
+                switch (attendanceStatus) {
+                    case 1:
+                        System.out.println("Day " + (totalWorkingDays + 1) + ": Employee is Present - Full Time");
+                        hoursWorked = company.getFullTimeHours();
+                        break;
+                    case 2:
+                        System.out.println("Day " + (totalWorkingDays + 1) + ": Employee is Present - Part Time");
+                        hoursWorked = company.getPartTimeHours();
+                        break;
+                    default:
+                        System.out.println("Day " + (totalWorkingDays + 1) + ": Employee is Absent");
+                }
+
+                totalWage += calculateDailyWage(hoursWorked, company.getHourlyWage());
+                totalWorkingHours += hoursWorked;
+                totalWorkingDays++;
             }
 
-            totalWage += calculateDailyWage(hoursWorked);
-            totalWorkingHours += hoursWorked;
-            totalWorkingDays++;
+            System.out.println("Total Monthly Employee Wage: " + totalWage);
+            System.out.println("Total Working Hours: " + totalWorkingHours);
+            System.out.println("Total Working Days: " + totalWorkingDays);
+            System.out.println("-------------------------");
         }
-
-        System.out.println("Total Monthly Employee Wage: " + totalWage);
-        System.out.println("Total Working Hours: " + totalWorkingHours);
-        System.out.println("Total Working Days: " + totalWorkingDays);
     }
 }
 
@@ -84,16 +122,13 @@ public class employeeWageComp {
         System.out.println(" Welcome to Employee Wage Computation Program ");
         System.out.println("----------------------------------------------");
 
-        EmpWageBuilder companyA = new EmpWageBuilder(100, 20, 8, 4, 20);
-        System.out.println("-----------------------------------------");
-        System.out.println("| Monthly Wage Computation for Company A |");
-        System.out.println("-----------------------------------------");
-        companyA.computeMonthlyWage();
+        CompanyEmpWage[] companies = {
+                new CompanyEmpWage("Company A", 100, 20, 8, 4, 20),
+                new CompanyEmpWage("Company B", 120, 25, 9, 5, 25)
+                // Add more companies as needed
+        };
 
-        EmpWageBuilder companyB = new EmpWageBuilder(120, 25, 9, 5, 25);
-        System.out.println("-----------------------------------------");
-        System.out.println("| Monthly Wage Computation for Company B |");
-        System.out.println("-----------------------------------------");
-        companyB.computeMonthlyWage();
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder(companies);
+        empWageBuilder.computeMonthlyWage();
     }
 }
